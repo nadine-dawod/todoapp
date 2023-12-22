f8a78609cf4f3a54ade274b968268c664396a39d
Nadine Dawod <127306010+nadine-dawod@users.noreply.github.com>
2023-12-22 19:20:51 +0100

    creating text for when not having any items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import Card from "./Card";
 import Checkbox from "./Checkbox";
-import {MdDeleteForever, MdEdit, MdOtlineCalendarToday, MdOutlineCalendarToday} from "react-icons/md";
+import {MdDeleteForever, MdEdit, MdOutlineCalendarToday} from "react-icons/md";
 
 
-const Item = ({ todo }) => {
+const Item = ({ todo, handleDelete, editTodo }) => {
 
     const [done, setDone] = useState(todo.done);
 
     const handleChange = (e) => {
         setDone(e.target.checked);
     }
+
+
 
 /*     const [title, setTitle] = useState("Title");
     const [task, setTask] = useState("Task"); */
@@ -21,15 +23,21 @@ const Item = ({ todo }) => {
             <div className="flex-container">
                 <div className="title">{todo.title}</div>
                 <div className="btns">
-                    <MdEdit style={{color: "#f7cd62", marginRight: ".5rem"}}/>
-                    <MdDeleteForever style={{color: "#ed844f"}} />  
+                    <MdEdit 
+                    style={{color: "#f7cd62", marginRight: ".5rem"}} 
+                    onClick={() => handleDelete(todo.id)}/>
+
+                    <MdDeleteForever 
+                    style={{color: "#ed844f"}} 
+                    onClick={() => editTodo(todo)}/>  
                 </div>
             </div>
             
             <div className="task">{todo.task}</div>
             <div className="flex-container"> 
                 <div className="day">
-                    <MdOutlineCalendarToday className="calendar"/>
+                    <MdOutlineCalendarToday 
+                    className="calendar"/>
                     <p>{todo.day}</p>
                 </div>
 
--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,11 @@ import Item from "./Item";
 
 
 const List = ({ todo }) => {
+
+    if (!todo || todo.length === 0) {
+        return <p>No ToDo's yet!</p>
+    }
+
     return (
     <div className="todo-list">
         {todo.map((item) => (
